d994430ff71064d76deee22cb17873263bed2462
Deayn Sadinov <sadinov88@gmail.com>
2014-12-16 12:28:20 +0200

    delete comments

--- src/test4/ProgessListener.java
+++
@@ -1,9 +0,0 @@
-package test4;
-
-/**
- * @author Deyan Sadinov <sadinov88@gmail.com>
- */
-public interface ProgessListener {
-
-  void onProgressUpdate(int savedBytes);
-}
--- src/test5/ClientMessageListener.java
+++
@@ -1,9 +0,0 @@
-package test5;
-
-/**
- * @author Deyan Sadinov <sadinov88@gmail.com>
- */
-public interface ClientMessageListener {
-
-  void onNewMessageReceived(String message);
-}
